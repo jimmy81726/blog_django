7cf6b1366b0fc39233c0737892e5d672ccf21e19
jimmy81726 <jimmy81726@gmail.com>
2023-11-11 14:56:05 +0800

    改變文章類型的select選取方法

--- post_comment/forms.py
+++ post_comment/forms.py
@@ -2,12 +2,12 @@ from django import forms
 from .models import Post, Category, Comment
 
 # 把category的資料表抓過來,[('Category1', 'Category1'), ('Category2', 'Category2')],左邊為存在數據庫的值,右邊為顯示的值
-choices = Category.objects.all().values_list("name", "name")
+# choices = Category.objects.all().values_list("name", "name")
 
-# 把querylist更改成一般list
-choices_list = []
-for i in choices:
-    choices_list.append(i)
+# # 把querylist更改成一般list
+# choices_list = []
+# for i in choices:
+#     choices_list.append(i)
 
 
 class CommentForm(forms.ModelForm):
@@ -32,6 +32,14 @@ class CommentForm(forms.ModelForm):
 # 發布文章所用的form繼承自Post model
 class PostForm(forms.ModelForm):
     # 在Meta下寫是針對內部已有的更新
+    # 寫進問題討論
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        widget=forms.Select(attrs={"class": "form-select"}),
+        label="文章類型",
+        empty_label="請選擇",
+    )
+
     class Meta:
         model = Post
         fields = ("title", "author", "category", "images", "content")
@@ -48,9 +56,9 @@ class PostForm(forms.ModelForm):
                     "type": "hidden",
                 }
             ),
-            "category": forms.Select(
-                choices=choices_list, attrs={"class": "form-select"}
-            ),
+            # "category": forms.Select(
+            #     choices=choices_list, attrs={"class": "form-select"}
+            # ),
             "content": forms.Textarea(
                 attrs={"class": "form-control", "placeholder": "請輸入內容"}
             ),
@@ -66,6 +74,13 @@ class PostForm(forms.ModelForm):
 
 # 修改文章所用的form繼承自Post model
 class EditForm(forms.ModelForm):
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        widget=forms.Select(attrs={"class": "form-select"}),
+        label="文章類型",
+        empty_label="請選擇",
+    )
+
     class Meta:
         model = Post
         fields = ("title", "category", "content")
@@ -73,9 +88,9 @@ class EditForm(forms.ModelForm):
             "title": forms.TextInput(
                 attrs={"class": "form-control", "placeholder": "請輸入標題"}
             ),
-            "category": forms.Select(
-                choices=choices_list, attrs={"class": "form-select"}
-            ),
+            # "category": forms.Select(
+            #     choices=choices_list, attrs={"class": "form-select"}
+            # ),
             "content": forms.Textarea(
                 attrs={"class": "form-control", "placeholder": "請輸入評論"}
             ),
--- post_comment/migrations/0004_category_post_category_alter_post_author.py
+++ post_comment/migrations/0004_category_post_category_alter_post_author.py
@@ -6,28 +6,37 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('post_comment', '0003_alter_post_author'),
+        ("post_comment", "0003_alter_post_author"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Category',
+            name="Category",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=255)),
             ],
         ),
         migrations.AddField(
-            model_name='post',
-            name='category',
-            field=models.CharField(default='數位生活', max_length=255),
+            model_name="post",
+            name="category",
+            field=models.CharField(default="數位生活", max_length=255),
         ),
         migrations.AlterField(
-            model_name='post',
-            name='author',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            model_name="post",
+            name="author",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL
+            ),
         ),
     ]
